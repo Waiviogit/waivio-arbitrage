4a4550a251e5879d584997084b560b6513054495
fesmofet <borees2@gmail.com>
2022-07-07 14:32:45 +0300

    add swagger cors, class validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.setGlobalPrefix('arbitrage');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      stopAtFirstError: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Arbitrage service')
+    .setDescription('Arbitrage service')
+    .setVersion('1.0')
+    .addTag('arbitrage')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('arbitrage/docs', app, document);
+  await app.listen(5000);
 }
 bootstrap();
