e21c7fa65e819a942143e5abb59e68da51399132
fesmofet <borees2@gmail.com>
2022-08-03 10:29:10 +0300

    fix fixed

--- src/api/controllers/profit/profit.service.ts
+++ src/api/controllers/profit/profit.service.ts
@@ -20,20 +20,12 @@ export class ProfitService {
   async addTokenToReport(
     params: EditReportInterface,
   ): Promise<InitialHoldingsDocumentType> {
-    params.quantity = new BigNumber(params.quantity).toFixed(
-      DEFAULT_PRECISION,
-      BigNumber.ROUND_HALF_UP,
-    );
     return this.profitReport.addTokenToReport(params);
   }
 
   async editQuantity(
     params: EditReportInterface,
   ): Promise<InitialHoldingsDocumentType> {
-    params.quantity = new BigNumber(params.quantity).toFixed(
-      DEFAULT_PRECISION,
-      BigNumber.ROUND_HALF_UP,
-    );
     return this.profitReport.editTokenQuantity(params);
   }
 
